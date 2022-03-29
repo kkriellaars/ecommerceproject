bbdf82fdddd43563d1ff549fcabd786b0922e982
Kyle Kriellaars <kkriellaars@academic.rrc.ca>
2022-03-29 14:19:40 -0500

    active storage install

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many :order, through: :order_detail
+  has_many :order, through: :order_details
   has_one_attached :image, dependent: :destroy
 end
